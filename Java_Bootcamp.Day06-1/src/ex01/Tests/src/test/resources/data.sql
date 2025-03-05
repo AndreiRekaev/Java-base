4abee80699498eac8876809af67ba407ac867e67
AndreiRekaev <96345021+AndreiRekaev@users.noreply.github.com>
2025-03-06 00:19:21 +0300

    Add files via upload

---
+++ Java_Bootcamp.Day06-1/src/ex01/Tests/src/test/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO shop.product(ID, NAME, PRICE) VALUES
+        (1, 'Meat', 350),
+        (2, 'Milk', 70),
+        (3, 'Spaghetti', 130),
+        (4, 'Bread', 60),
+        (5, 'Potato', 80);
---
+++ Java_Bootcamp.Day06-1/src/ex03/Tests/src/test/resources/data1.sql
@@ -0,0 +1,6 @@
+INSERT INTO service.user(id, login, password, AUTHENTICATED) VALUES
+                                              (1, 'user1', '1111', false),
+                                              (2, 'user2', '2222', false),
+                                              (3, 'user3', '3333', false),
+                                              (4, 'user4', '4444', false),
+                                              (5, 'user5', '5555', false);
