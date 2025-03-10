fa35c7c599d35d5fa0dacf97c8560f97f9942343
andrei <andrei@Ubuntu.andrei.virtualbox.org>
2025-03-10 17:44:20 +0300

    release

---
+++ exceptions/My_Exception/src/main/java/org/example/InvalidTypeException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+class InvalidTypeException extends Exception {
+  public InvalidTypeException(String message) {
+    super(message);
+  }
+}
---
+++ exceptions/My_Exception/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        CollectionHandler handler = new CollectionHandler();
+
+        handler.checkCorrectElem(10);
+        handler.checkCorrectElem("Hello");
+    }
+}
